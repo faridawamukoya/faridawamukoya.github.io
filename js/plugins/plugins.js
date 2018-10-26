3e0566fa1b07db52ea52e8333d62bd8d8c776d4f
faridawamukoya <faridawamukoya@gmail.com>
2018-10-26 19:13:55 +0300

    changed the footer image

--- js/plugins/plugins.js
+++ js/plugins/plugins.js
@@ -1,3 +1,4 @@
+//# sourceMappingURL=http://example.com/path/to/your/sourcemap.map
 /*! jQuery v1.10.2 | (c) 2005, 2013 jQuery Foundation, Inc. | jquery.org/license
 //@ sourceMappingURL=jquery-1.10.2.min.map
 */
